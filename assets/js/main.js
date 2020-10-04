95a540c041574bf067829087e46305a08eaffec7
ttuseong <zlsp21128@naver.com>
2020-10-04 12:35:21 +0900

    교육 항목에 대한 틀 완성, 메뉴 바 완성

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,44 @@
+//각 메뉴들이 이동할 크기
+var viewSize;
+//바의 크기, 한칸당 이동할 거리로 사용
+var barSize;
+//현재 위치
+var pos;
+//교육 내용들의 크기
+var eduList=[];
+
 // 리스트 세로 정렬
 $(".listItem").css("line-height", $("#listRow").css("height"));
 
-var viewSize;
-var pos;
-
 //페이지에 들어올 때 사용자 화면의 크기에 맞게 조절
 $(document).ready(function(){
-  console.log("test");
   sizing();
+  educateListInit();
 });
 
 //사용자가 브라우저의 크기를 변경할 경우
 $(window).resize(function(){
-  console.log("가능!?");
   sizing();
 });
 
+//메뉴클릭
 $(".listItem").on("click", function(){
-  console.log("슬라이드 테스트")
   pos = $(this).data("no");
 
   $("#mainContents").animate({
-			marginLeft : viewSize * pos * -1
-		});
+		marginLeft : viewSize * pos * -1
+	});
 
+  $("#bar").animate({
+    marginLeft : barSize * pos * 1
+  });
 });
 
 //메인 컨텐츠들의 화면 크기를 정해주는 함수
 function sizing(){
-  viewSize = Number($("#mainContainer").css("width").split('px')[0])
+  viewSize = Number($("#mainContainer").css("width").split('px')[0]);
+  barSize = Number($("#bar").css("width").split('px')[0]);
+
   $("#mainContents").css("width", viewSize*4);
 
   var target = $(".contentSize");
@@ -38,4 +48,34 @@ function sizing(){
   }
 
   $("#mainContents").css("margin-left", viewSize * pos * -1);
+  $("#bar").css("margin-left", barSize * pos * 1);
 }
+
+function educateListInit(){
+  var length = $(".educateList").length;
+
+  for(var i = 0; i < length; i++){
+    eduList.push(Number($(".educateList").eq(i).css("height").split('px')[0]));
+    $(".educateList").eq(i).css("height", 0);
+    $(".educateList").eq(i).data("no", i);
+  }
+
+  console.log(eduList);
+}
+
+//교육 내용
+$(".educateBtn").on("click", function(){
+  var target = $(this).next();
+
+  if(target.css("height").split('px')[0] == 0){
+    var index = target.data("no");
+
+    target.animate({
+      height : eduList[index]
+    });
+  } else{
+    target.animate({
+      height : 0
+    });
+  }
+});
